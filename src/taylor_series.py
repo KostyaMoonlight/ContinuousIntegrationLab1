7f6e1855019aa4bf482802e4af1b16c60afabfc3
Kostya Zdor <k.zdor@cvrndteam.com>
2020-11-11 21:56:31 +0200

    [Fix] function name.

--- src/taylor_series.py
+++ src/taylor_series.py
@@ -3,7 +3,7 @@ from functools import reduce
 
 class TaylorSeries:
     
-    def exponential_function(self, value):
+    def exponential_functionA(self, value):
         return sum([float(value)**n/self.__factorial(n) for n in range(4)])
 
     def __factorial(self, value):
