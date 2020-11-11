15144197b3dd3342132d0a0d2f7c67dc91a8962f
Kostya Zdor <k.zdor@cvrndteam.com>
2020-11-11 21:19:08 +0200

    [Add] Taylor series.

---
+++ src/taylor_series.py
@@ -0,0 +1,10 @@
+__version__="1.0.0.0"
+from functools import reduce
+
+class TaylorSeries:
+    
+    def exponential_function(self, value):
+        return sum([float(value)**n/self.__factorial(n) for n in range(4)])
+
+    def __factorial(self, value):
+        return reduce((lambda x,y:x*y),list(range(1,value+1)), 1)
